836048497bf28459b6c7c2497ce52d1ab89789ef
ohryan <ryanachacon@gmail.com>
2020-12-04 16:13:51 -0700

    fix mv

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const exec = require('child_process').execSync
 
-const name = 'Gesa\ DJ\ set\ Boiler\ Room\ Berlin.mp4'
+const name = 'Gesa Boiler Room.mp4'
 const url = 'https://www.youtube.com/watch?v=8k4UzJnH504'
 const surdir = 'Music'
 const subdir = 'downloads'
@@ -14,7 +14,7 @@ res.on('finish', () => {
 	try {
 		exec(`mkdir ~/${surdir}/${subdir}`)
 	} catch (_) {}
-	exec(`mv "${name}" ~/${surdir}/${subdir}/`)
+	exec(`mv "./downloads/${name}" ~/${surdir}/${subdir}/`)
 	console.log('done')
 	process.exit()
 })
