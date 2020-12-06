6565604f4ab9478455e573d956435c73534015cf
ohryan <ryanachacon@gmail.com>
2020-12-06 11:47:17 -0700

    download just mp3 via cmd line args

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const exec = require('child_process').execSync
+// need to install ffmpeg on your system first
+// brew install ffmpeg
+const ffmpeg = require('fluent-ffmpeg')
 
-const name = 'Gesa Boiler Room.mp4'
-const url = 'https://www.youtube.com/watch?v=8k4UzJnH504'
-const surdir = 'Music'
-const subdir = 'downloads'
+const [node, pwd, url, name, ext] = process.argv
+const downloadPath = `./downloads/${name}.${ext || 'mp3'}`
 
-const res = ytdl(url)
-	.pipe(fs.createWriteStream(`./downloads/${name}`))
+const stream = ytdl(url)
+const proc = new ffmpeg({ source: stream })
+proc.setFfmpegPath('/usr/local/bin/ffmpeg').withAudioBitrate('192k')
 
-res.on('finish', () => {
-	try {
-		exec(`mkdir ~/${surdir}/${subdir}`)
-	} catch (_) {}
-	exec(`mv "./downloads/${name}" ~/${surdir}/${subdir}/`)
-	console.log('done')
+const interval = setInterval(() => {
+	console.log('Processing...')}, 3000)
+
+proc.saveToFile(downloadPath)
+proc.on('error', err => {
+	console.log('Error', err)
+	clearTimeout(interval)
+	process.exit()
+})
+proc.on('end', () => {
+	clearTimeout(interval)
+	console.log('Done')
 	process.exit()
 })
 
