64c158c5efea272b6995912ee009909b9de9e7c4
ohryan <ryanachacon@gmail.com>
2021-03-05 17:02:39 -0700

    update to download multiple tracks asyncronously

--- main.js
+++ main.js
@@ -1,29 +1,71 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const exec = require('child_process').execSync
+
 // need to install ffmpeg on your system first
 // brew install ffmpeg
 const ffmpeg = require('fluent-ffmpeg')
 
-const [node, pwd, url, name, ext] = process.argv
-const downloadPath = `./downloads/${name}.${ext || 'mp3'}`
+const [node, pwd, dirname, ext] = process.argv
 
-const stream = ytdl(url)
-const proc = new ffmpeg({ source: stream })
-proc.setFfmpegPath('/usr/local/bin/ffmpeg').withAudioBitrate('192k')
+const errors = new Map()
 
-const interval = setInterval(() => {
-	console.log('Processing...')}, 3000)
+function printErrorMap () {
+	const arr = []
+	errors.forEach((url, name) => { arr.push([name, url]) })
+	if (arr.length !== 0) {
+		console.log('Error map:\n', JSON.stringify(arr, null, 2))		
+	}
+}
 
-proc.saveToFile(downloadPath)
-proc.on('error', err => {
-	console.log('Error', err)
-	clearTimeout(interval)
-	process.exit()
-})
-proc.on('end', () => {
-	clearTimeout(interval)
+const tracksToDownload = require('./downloadTracks.js')
+
+const downloadDir = `./downloads/${dirname || 'test'}`
+if (!fs.existsSync(downloadDir)){
+   fs.mkdirSync(downloadDir);
+}
+
+async function download (name, url) {
+	return new Promise((res, rej) => {
+		console.log('Downloading: ', name, url)
+		const downloadPath = `${downloadDir}/${name}.${ext || 'mp3'}`
+	
+		const stream = ytdl(url)
+		const proc = new ffmpeg({ source: stream })
+
+		proc.setFfmpegPath('/usr/local/bin/ffmpeg').withAudioBitrate('192k')
+		proc.saveToFile(downloadPath)
+
+		proc.on('error', err => {
+			console.error(`Error downloading ${name}: ${err}`)
+			errors.set(name, url)
+			res([name, url])
+		})
+
+		proc.on('end', () => {
+			console.log(`Downloaded ${name}`)
+			res([name, url])
+		})
+	})
+	console.log('?')
+}
+
+async function downloadAll () {
+	const promises = []
+	for (const track of tracksToDownload) {
+		const [name, url] = track
+		promises.push(download(name, url))
+	}
+
+	const interval = setInterval(() => {
+		console.log('Processing...')}, 3000)
+
+	await Promise.all(promises)
+
+	clearInterval(interval)
 	console.log('Done')
+	printErrorMap()
 	process.exit()
-})
+}
 
+downloadAll()
